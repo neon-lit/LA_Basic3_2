5ecb99b5d3692a4d82315afa12cfbc250ebd529d
EnonTaira <taira.e2007@gmail.com>
2026-08-26 03:42:00 +0900

    Initial commit

---
+++ List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  List
+//
+//  Created by Taira Enon on 2026/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let array = ["いぬ", "ねこ", "さる", "とり"]
+    var body: some View {
+        VStack {
+            List{
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ List/ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by Taira Enon on 2026/08/26.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
